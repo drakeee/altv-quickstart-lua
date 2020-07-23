d0ab222e8068468a3aeffa2879df7188eb60f632
drakeee <drakedrift@gmail.com>
2020-07-23 14:52:36 +0200

    Fix some commands.

--- example/server/commands/respawn.lua
+++ example/server/commands/respawn.lua
@@ -2,11 +2,11 @@ require('systems.chat')
 require('configuration.config')
 require('utility.vector')
 
-registerCmd('respawn', '/respawn | Respawn the player at spawn.', handleRespawn)
-registerCmd('spawn', '/spawn | Respawn the player at spawn.', handleRespawn)
 
 function handleRespawn(player)
     local randomPosition = randomPositionAround(DEFAULT_CONFIG.SPAWN, DEFAULT_CONFIG.SPAWN_RANGE)
     player:spawn(randomPosition.x, randomPosition.y, randomPosition.z, 0)
     player:send('You were respawned.')
 end
+registerCmd('respawn', '/respawn | Respawn the player at spawn.', handleRespawn)
+registerCmd('spawn', '/spawn | Respawn the player at spawn.', handleRespawn)
